7e5559f8ceca30359db13aa54000c174efa2ae50
Lindy09 <99447406+Lindy09@users.noreply.github.com>
2025-10-01 20:41:41 +0200

    Update QuestionOne.cs

--- Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
@@ -4,8 +4,14 @@ namespace Inoxico.TechnicalQuestions.Answers
     public class QuestionOne
     {
         public static int GetLongestSentance(string s)
-        {
-            throw new NotImplementedException();
+        { 
+            s = "We test coders. Give us a try?";
+            		string[] words = s.Split(' ', '.','?');
+               
+			foreach (var word in words)
+				{
+    			Console.WriteLine(word);
+				}    
         }
     }
 }

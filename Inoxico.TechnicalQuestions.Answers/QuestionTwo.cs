e436bf4d2522aedc030b97fae3217c6cbb9758f8
Lindy09 <99447406+Lindy09@users.noreply.github.com>
2025-10-08 06:29:40 +0200

    Update QuestionTwo.cs

--- Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
@@ -5,7 +5,28 @@ namespace Inoxico.TechnicalQuestions.Answers
     {
         public static int GetPitDepth(int[] points)
         {
-            throw new NotImplementedException();
+             int n = points.Length;
+             int maxDepth = -1;
+
+            while (i < n - 1)
+            {
+                while (i < n - 1 && points[i] <= points[i + 1])
+                    i++;
+                
+                int P = i;
+                while (i < n - 1 && points[i] > points[i + 1])
+                    i++;
+                int Q = i;
+                while (i < n - 1 && points[i] < points[i + 1])
+                    i++;
+                int R = i;
+                if (P < Q && Q < R && points[P] > 0)
+                {
+                    int depth = Math.Min(points[P] - points[Q], points[R] - points[Q]);
+                    maxDepth = Math.Max(maxDepth, depth);
+                }
+            }
+            return maxDepth;
         }
     }
 }
